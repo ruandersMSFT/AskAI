f9b47d418758b45b25cfdfcea1478e4574baa3cb
ruandersMSFT <russell@russellanderson.net>
2024-03-12 16:56:04 -0600

    azurerm_role_assignment

--- main.tf
+++ main.tf
@@ -175,7 +175,7 @@ resource "azurerm_linux_web_app" "web" {
     "COSMOSDB_URL"                          = azurerm_cosmosdb_account.example.endpoint
     "EMBEDDING_DEPLOYMENT_NAME"             = "text-embedding-ada-002"
     "ENABLE_ORYX_BUILD"                     = "True"
-    "ENRICHMENT_APPSERVICE_NAME"            = "infoasst-enrichmentweb-geprk"
+    "ENRICHMENT_APPSERVICE_NAME"            = azurerm_linux_web_app.enrichment.name
     "IS_GOV_CLOUD_DEPLOYMENT"               = "False"
     "QUERY_TERM_LANGUAGE"                   = "English"
     "SCM_DO_BUILD_DURING_DEPLOYMENT"        = "true"
@@ -849,4 +849,77 @@ resource "azurerm_key_vault_access_policy" "current_user" {
     "Restore",
     "Set",
   ]
+}
+
+resource "azurerm_role_assignment" "user_search_openai" {
+  scope                = azurerm_resource_group.example.id
+  role_definition_name = "Cognitive Services OpenAI User" # 5e0bd9bd-7b93-4f28-af87-19fc36ad61bd
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_role_assignment" "user_storage_reader" {
+  scope                = azurerm_resource_group.example.id
+  role_definition_name = "Storage Blob Data Reader" # 2a2b9908-6ea1-4ae2-8e65-a410df84e7d1
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_role_assignment" "user_storage_contributor" {
+  scope                = azurerm_resource_group.example.id
+  role_definition_name = "Storage Blob Data Contributor" # ba92f5b4-2d11-453d-a403-e96b0029c9fe
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_role_assignment" "user_search_reader" {
+  scope                = azurerm_resource_group.example.id
+  role_definition_name = "Search Index Data Reader" # 1407120a-92aa-4202-b7e9-c0e197c71c8f
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_role_assignment" "user_search_contributor" {
+  scope                = azurerm_resource_group.example.id
+  role_definition_name = "Search Index Data Contributor" # 8ebe5a00-799e-43f5-93ac-243d3dce84a7
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+# SYSTEM IDENTITIES
+resource "azurerm_role_assignment" "backend_openai" {
+  scope                = azurerm_resource_group.example.id
+  role_definition_name = "Cognitive Services OpenAI User" # 7f951dda-4ed3-4680-a7ca-43fe172d538d
+  principal_id         = azurerm_linux_web_app.web.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "webapp_acrpull" {
+  scope                = azurerm_resource_group.example.id
+  role_definition_name = "AcrPull" # 5e0bd9bd-7b93-4f28-af87-19fc36ad61bd
+  principal_id         = azurerm_linux_web_app.enrichment.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "backend_storage" {
+  scope                = azurerm_resource_group.example.id
+  role_definition_name = "Storage Blob Data Reader" # 2a2b9908-6ea1-4ae2-8e65-a410df84e7d1
+  principal_id         = azurerm_linux_web_app.web.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "backend_search" {
+  scope                = azurerm_resource_group.example.id
+  role_definition_name = "Search Index Data Reader" # 1407120a-92aa-4202-b7e9-c0e197c71c8f
+  principal_id         = azurerm_linux_web_app.web.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "function_storage" {
+  scope                = azurerm_resource_group.example.id
+  role_definition_name = "Storage Blob Data Reader" # 2a2b9908-6ea1-4ae2-8e65-a410df84e7d1
+  principal_id         = azurerm_linux_function_app.example.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "aad_acrpush" {
+  scope                = azurerm_resource_group.example.id
+  role_definition_name = "AcrPush" # 8311e382-0749-4cb8-b61a-304f252e45ec
+  principal_id         = local.azure_client_id
+}
+
+resource "azurerm_role_assignment" "aad_openai" {
+  scope                = azurerm_resource_group.example.id
+  role_definition_name = "Cognitive Services OpenAI User" # 5e0bd9bd-7b93-4f28-af87-19fc36ad61bd
+  principal_id         = local.azure_client_id
 }
