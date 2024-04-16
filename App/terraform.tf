b46ac411106a313e4717720595be279491dbf46a
ruandersMSFT <russell@russellanderson.net>
2024-04-16 16:52:31 -0400

    Azure/azapi Provider

--- App/terraform.tf
+++ App/terraform.tf
@@ -1,10 +1,17 @@
 terraform {
   required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+      version = "=1.12.1"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "=3.99.0"
     }
   }
+}
+
+provider "azapi" {
 }
 
 provider "azurerm" {
